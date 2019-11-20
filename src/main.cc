7ef246881e7a21d2ebec2d34dee8dbcbca584b2f
Krishna Kumar <krishnak@utexas.edu>
2019-11-20 09:14:38 -0600

    :construction: THM code

---
+++ src/main.cc
@@ -0,0 +1,40 @@
+#include "geothermal.h"
+
+int main(int argc, char *argv[])
+{
+  try
+  {
+    using namespace dealii;
+
+    Goethermal<3> geothermal;
+    geothermal.run();
+  }
+  catch (std::exception &exc)
+  {
+    std::cerr << std::endl
+              << std::endl
+              << "----------------------------------------------------"
+              << std::endl;
+    std::cerr << "Exception on processing: " << std::endl
+              << exc.what() << std::endl
+              << "Aborting!" << std::endl
+              << "----------------------------------------------------"
+              << std::endl;
+
+    return 1;
+  }
+  catch (...)
+  {
+    std::cerr << std::endl
+              << std::endl
+              << "----------------------------------------------------"
+              << std::endl;
+    std::cerr << "Unknown exception!" << std::endl
+              << "Aborting!" << std::endl
+              << "----------------------------------------------------"
+              << std::endl;
+    return 1;
+  }
+
+  return 0;
+}
