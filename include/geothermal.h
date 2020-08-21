35a5e5186623476c432bd7c1368941311af39e1b
shyaan <shyaansun@outlook.com>
2020-08-20 17:50:41 -0700

    add more building piles

--- include/geothermal.h
+++ include/geothermal.h
@@ -172,7 +172,7 @@ void CoupledTH<dim>::make_grid() {
 
   GridIn<dim> gridin;  // instantiate a gridinput
   gridin.attach_triangulation(triangulation);
-  std::ifstream f("inputfiles/denseisland.msh");
+  std::ifstream f("inputfiles/new_build.msh");
   gridin.read_msh(f);
   // print_mesh_info(triangulation, "outputfiles/grid-1.eps");
   // triangulation.refine_global(1);
