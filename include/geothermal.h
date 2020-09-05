eab155f0021abf1b3ec3362d192d8335be525fd2
shyaan <shyaansun@outlook.com>
2020-09-04 23:40:08 -0700

    delete if statement

--- include/geothermal.h
+++ include/geothermal.h
@@ -70,15 +70,9 @@
 #include "interpolation.h"
 
 namespace LA {
-#if defined(DEAL_II_WITH_PETSC) && !defined(DEAL_II_PETSC_WITH_COMPLEX) && \
-    !(defined(DEAL_II_WITH_TRILINOS) && defined(FORCE_USE_OF_TRILINOS))
 using namespace dealii::LinearAlgebraPETSc;
 #define USE_PETSC_LA
-#elif defined(DEAL_II_WITH_TRILINOS)
-using namespace dealii::LinearAlgebraTrilinos;
-#else
-#error DEAL_II_WITH_PETSC or DEAL_II_WITH_TRILINOS required
-#endif
+
 }  // namespace LA
 
 using namespace dealii;
